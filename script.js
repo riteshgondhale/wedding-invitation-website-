eca6bc9fe621002d5dcef9f55cd276bec6977837
riteshgondhale <riteshgondhale007@gmail.com>
2025-02-27 16:32:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ function updateCountdown() {
     const timeLeft = weddingDate - now;
 
     const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    const hr = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const min = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const sec = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
     document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
+    document.getElementById("hours").innerText = hr;
+    document.getElementById("minutes").innerText = min;
+    document.getElementById("seconds").innerText = sec;
 }
 
 setInterval(updateCountdown, 1000);
